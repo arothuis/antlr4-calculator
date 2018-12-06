8cfdab1514cc141c57101f7be0e3a42cedcc6c23
Alex Rothuis <alex.rothuis@hu.nl>
2018-12-06 14:19:52 +0100

    Add CalculationListener

---
+++ src/main/java/parser/CalculationListener.java
@@ -0,0 +1,74 @@
+package parser;
+
+import java.util.Stack;
+
+public class CalculationListener extends CalculatorBaseListener {
+    /**
+     * A stack is used to track what numbers
+     * we have seen last. This is our only
+     * means of communicating values.
+     *
+     * The right number in
+     * an operation is always the last one seen or
+     * calculated for that operation. Because a stack
+     * works via Last In First Out,
+     * the right number should be "popped" off first.
+     */
+    private Stack<Double> stack = new Stack<>();
+
+    @Override
+    public void exitNumber(CalculatorParser.NumberContext ctx) {
+        Double number = Double.parseDouble(ctx.NUMBER().getText());
+        this.stack.push(number);
+    }
+
+    @Override
+    public void exitPower(CalculatorParser.PowerContext ctx) {
+        Double right = this.stack.pop();
+        Double left = this.stack.pop();
+
+        this.stack.push(Math.pow(left, right));
+    }
+
+    @Override
+    public void exitMultiplication(CalculatorParser.MultiplicationContext ctx) {
+        Double right = this.stack.pop();
+        Double left = this.stack.pop();
+
+        this.stack.push(left * right);
+    }
+
+    @Override
+    public void exitDivision(CalculatorParser.DivisionContext ctx) {
+        Double right = this.stack.pop();
+        Double left = this.stack.pop();
+
+        this.stack.push(left / right);
+    }
+
+    @Override
+    public void exitAddition(CalculatorParser.AdditionContext ctx) {
+        Double right = this.stack.pop();
+        Double left = this.stack.pop();
+
+        this.stack.push(left + right);
+    }
+
+    @Override
+    public void exitSubtraction(CalculatorParser.SubtractionContext ctx) {
+        Double right = this.stack.pop();
+        Double left = this.stack.pop();
+
+        this.stack.push(left - right);
+    }
+
+    /**
+     * The last value on the stack is the result of all
+     * applied calculations.
+     *
+     * @return Double
+     */
+    public Double getResult() {
+        return this.stack.pop();
+    }
+}
