dd026556a936fafbec31ac4fe4f9da90aae37a71
Alex Rothuis <alex.rothuis@gmail.com>
2023-04-05 23:21:31 +0200

    chore: cleanup test

--- src/test/java/nl/arothuis/antlr4calculator/core/calculator/ListenerBasedCalculatorTest.java
+++ src/test/java/nl/arothuis/antlr4calculator/core/calculator/ListenerBasedCalculatorTest.java
@@ -2,12 +2,9 @@ package nl.arothuis.antlr4calculator.core.calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ListenerBasedCalculatorTest {
     @DisplayName("evaluate simple mathematical expressions")
